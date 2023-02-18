64358821599dca30fb6122d3231565fc95e76060
Kirukle <113458461+Kirukle@users.noreply.github.com>
2023-02-17 21:23:22 -0800

    Stuff

--- PenProject/Book.cs
+++ PenProject/Book.cs
@@ -15,11 +15,13 @@ namespace PenProject
         public float Price;
         public int AmountOfPages;
         public int CurrentPage;
+        public int OriginalPage;
 
         //Constructor
 
 
-        public Book(string name, string cover, bool hasBookMark, bool isOpen, float length, float price, int amountOfPages, int currentPage)
+        public Book(string name, string cover, bool hasBookMark, bool isOpen, float length, float price,
+            int amountOfPages, int currentPage, int originalPage)
         {
             Name = name;
             Cover = cover;
@@ -29,37 +31,66 @@ namespace PenProject
             Price = price;
             AmountOfPages = amountOfPages;
             CurrentPage = currentPage;
+            OriginalPage = originalPage;
         }
+
+        public Book()
+        {
+        }
+
         public void Open()
         {
             IsOpen = true;
-            Console.WriteLine("You have opened {0}. You are on page {1}.", Name, );
+            if(HasBookMark == true)
+            {
+                Console.WriteLine("You have opened {0}. You are on page {1}.", Name, CurrentPage);
+            }
+            if (HasBookMark == false)
+            {
+                Console.WriteLine("You have opened {0}. You are on page {1}.", Name, CurrentPage);
+            }
+        }
+
+        public void Close()
+        {
+            IsOpen = false;
+            if (HasBookMark == true)
+            {
+                Console.WriteLine("You close the book on page {0}.", CurrentPage);
+            }
+            if (HasBookMark == false)
+            {
+                Console.WriteLine("You close the book on page {0}.", CurrentPage);
+                CurrentPage = OriginalPage;
+            }
         }
 
         public void Read()
         {
             if(IsOpen == true)
             {
-                if(HasBookMark == true)
-                {
-                    Console.WriteLine("You mark",)
-                }
-                else
-                {
-
-                }
-            }
-
+                CurrentPage++;
+                Console.WriteLine("You read a page, you are now on page {0}.", CurrentPage);
+            }else
             {
-                Console.WriteLine("You read the cover of {0}, it says {1} and has the price of {2}.", Name, Cover, Price);
+                Console.WriteLine("You read the cover of {0}, it says {1} and has the price of ${2}.", Name, Cover, Price);
             }
         }
 
-        public void BookMark()
+        public void BookMarkIn()
         {
+            HasBookMark = true;
+            Console.WriteLine("You have placed your bookmark on page {0}.", CurrentPage);
 
         }
 
-     
+        public void BookMarkOut()
+        {
+            Console.WriteLine("You have pulled out your bookmark.");
+            HasBookMark = false;
+        }
+
+
+
     }
 }
--- PenProject/Program.cs
+++ PenProject/Program.cs
@@ -2,27 +2,44 @@
 
 namespace PenProject
 {
-    class Program
+    public class Program
     {
        static void Main(String[] args)
         {
-            Book divGradCurlAndAllThat = new Book();
-            divGradCurlAndAllThat.Name = "DIV, GRAD, CURL, AND ALL THAT";
-            divGradCurlAndAllThat.Cover = "DIV, GRAD, CURL, AND ALL THAT By H.M SCHEY,  An Informal Text on Vector Calculus";
-            divGradCurlAndAllThat.HasBookMark = false;
-            divGradCurlAndAllThat.AmountOfPages = 162;
-            divGradCurlAndAllThat.Length = 13.5f;
-            divGradCurlAndAllThat.CurrentPage = 1;
-            divGradCurlAndAllThat.Price = 6.99f;
-            divGradCurlAndAllThat.IsOpen = false;
+            Book Crucible = new Book();
+            Crucible.Name = "DIV, GRAD, CURL, AND ALL THAT";
+            Crucible.Cover = "DIV, GRAD, CURL, AND ALL THAT By H.M SCHEY," +
+                "  An Informal Text on Vector Calculus";
+            Crucible.HasBookMark = false;
+            Crucible.AmountOfPages = 162;
+            Crucible.Length = 13.5f;
+            Crucible.CurrentPage = 1;
+            Crucible.Price = 6.99f;
+            Crucible.IsOpen = false;
 
-            //Book divGradCurlAndAllThat = new Book("DIV, GRAD, CURL, AND ALL THAT", "DIV, GRAD, CURL, AND ALL THAT By H.M SCHEY,  An Informal Text on Vector Calculus",
-            //    false, 162, 13.5f, 1, 6.99f, false);
-            
-            
-            
+            Book divGradCurlAndAllThat = new Book("DIV, GRAD, CURL, AND ALL THAT", "DIV, GRAD, CURL, AND ALL THAT By H.M SCHEY,  An Informal Text on Vector Calculus",
+               false, false, 13.5f, 6.99f, 162, 1, 1);
+
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.Open();
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.BookMarkIn();
+            divGradCurlAndAllThat.Close();
+            divGradCurlAndAllThat.Open();
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.BookMarkOut();
+            divGradCurlAndAllThat.Close();
+            divGradCurlAndAllThat.Open();
+            divGradCurlAndAllThat.Read();
+            divGradCurlAndAllThat.Read();
+
+
+
         }
 
-
+       
     }
 }
